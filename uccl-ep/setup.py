d6aeb2660c4d1412da4925b7bad4d61979b93f23
DanielDanyang <daniel1691029779@gmail.com>
2026-05-27 02:47:17 -0700

    Avoid libnuma dependency in uccl-ep build

--- uccl-ep/setup.py
+++ uccl-ep/setup.py
@@ -142,7 +142,7 @@ if __name__ == "__main__":
     ]
     nvcc_flags = ["-O3", "-Xcompiler", "-O3"]
     sources = glob("./src/*.cu") + glob("./src/*.cpp") + glob("./src/*.cc")
-    libraries = ["ibverbs", "nl-3", "nl-route-3", "numa"]
+    libraries = ["ibverbs", "nl-3", "nl-route-3"]
     include_dirs = [PROJECT_ROOT / "include", PROJECT_ROOT / ".." / "include"]
     # Nanobind stable-ABI bindings
     nb_dir = Path(nanobind.__file__).parent
